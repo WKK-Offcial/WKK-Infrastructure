20752d95ec4e588843a70ba26327831a81732e0c
paszymaja <sprzepiora@hotmail.co.uk>
2023-03-19 14:35:15 +0100

    install docker and docker compose

--- main.tf
+++ main.tf
@@ -33,6 +33,15 @@ resource "aws_instance" "boi_bot" {
   for ssh_key in "$${ssh_array[@]}"; do
   echo "$ssh_key" >> /home/ec2-user/.ssh/authorized_keys
   done
+
+  sudo yum update -y
+  sudo yum install docker -y
+  sudo service docker start
+  sudo usermod -a -G docker ec2-user
+  newgrp docker
+  sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  docker-compose version
   FILE
 
   tags = {

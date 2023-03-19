51e3c5cd406ee9d0ba416ec9094571d671d8a825
paszymaja <sprzepiora@hotmail.co.uk>
2023-03-19 11:33:13 +0100

    boi infra

--- main.tf
+++ main.tf
@@ -2,13 +2,41 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.58.0"
+      version = "~> 4.50"
     }
   }
+
+  required_version = ">= 1.4"
 }
 
 provider "aws" {
   region = "eu-central-1"
+}
+
+resource "tls_private_key" "ec2_ssh_key" {
+  algorithm = "RSA"
+}
+
+resource "aws_key_pair" "ec2_ssh_key" {
+  key_name   = "ec2_ssh_key"
+  public_key = tls_private_key.ec2_ssh_key.public_key_openssh
+}
+
+resource "aws_instance" "boi_bot" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.ec2_ssh_key.key_name
+  user_data     = <<FILE
+  #!/bin/bash
+  IFS=',' read -r -a ssh_array <<< "${join(",", var.ec2_ssh_public_keys)}"
+  for ssh_key in "$${ssh_array[@]}"; do
+  echo "$ssh_key" >> /home/ec2-user/.ssh/authorized_keys
+  done
+  FILE
+
+  tags = {
+    Name = "WKK-Bot"
+  }
 }
 
 data "aws_ami" "ubuntu" {
@@ -22,16 +50,5 @@ data "aws_ami" "ubuntu" {
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
   }
 }

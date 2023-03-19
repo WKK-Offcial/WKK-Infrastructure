e29ef3b208c2a7b4d3f01073016cb162d153a53d
paszymaja <sprzepiora@hotmail.co.uk>
2023-03-19 14:52:53 +0100

    cleanup

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
 output "instance_ip_addr" {
   value = aws_instance.boi_bot.public_ip
 }
-
-output "aws_private_key" {
-  value     = tls_private_key.ec2_ssh_key.private_key_pem
-  sensitive = true
-}

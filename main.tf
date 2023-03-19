6ec5de81a5c29961ceef57536ee20eb3d5d04c01
paszymaja <sprzepiora@hotmail.co.uk>
2023-03-19 14:52:04 +0100

    test script

--- main.tf
+++ main.tf
@@ -32,9 +32,9 @@ resource "aws_instance" "boi_bot" {
   newgrp docker
   sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
-  docker-compose version
   FILE
 
+  user_data_replace_on_change = true
   tags = {
     Name = "WKK-Bot"
   }
